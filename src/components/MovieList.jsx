7755af0953ab3497862c2bffb189cf9be3eeb226
Rob <rob.sakal23@gmail.com>
2026-02-26 12:04:41 -0500

    ratings issue

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -20,16 +20,29 @@ const MovieList = () => {
         }
     };
 
+    async function fetchMovieDetails(imdbID, apiKey) {
+            const url = `https://www.omdbapi.com/?apikey=${apiKey}&i=${imdbID}`;
+            const response = await axios.get(url);
+            return response.data;
+        }
+
+    function movieHTML(movie) {
+        const imdbRating = movie.Ratings.find(rating => rating.Source === "Internet Movie Database")?.Value || "N/A";
+        const rottenTomatoesRating = movie.Ratings.find(rating => rating.Source === "Rotten Tomatoes")?.Value || "N/A";
+    }
+
     return (
         <>
             <SearchBar onSearch={fetchMovies} />
             <Filter />
             <div className="movie__list">
                 {movies.map((movie) => (
-                    <div className="movie__card" key={movie.imdbID}>
+                    <div className="movie__card">
                         <img className="box__art" src={movie.Poster} alt={movie.Title} />
                           <h3 className='movie__title'>{movie.Title}</h3>
                           <p className='release__date'>Released {movie.Year}</p>
+                          <p className="imdb__page">IMDb Rating: {movie.imdbRating}</p>
+            <p className="rotten__tomatoes">Rotten Tomatoes: {movie.rottenTomatoesRating}</p>
                     </div>
                 ))}
             </div>
